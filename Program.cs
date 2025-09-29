ad6a9a0cf03fb48a5fce5c5e51c73dfbe4ac9441
Luiza Santos <luizahelena.lsantos@gmail.com>
2025-09-29 19:22:16 -0300

    feat: criação do bd

--- Program.cs
+++ Program.cs
@@ -1,8 +1,16 @@
+using Microsoft.EntityFrameworkCore;
+using movies_api.data;
+using SQLitePCL;
+
+Batteries.Init();
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
+builder.Services.AddDbContext<MoviesContext>(options =>
+    options.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection")));
 
 var app = builder.Build();
 
